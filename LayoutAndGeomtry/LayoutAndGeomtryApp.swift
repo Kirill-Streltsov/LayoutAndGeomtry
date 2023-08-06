14ef0ad18f3454ab278ee38605146e1e4d3f0b7d
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-08-06 23:10:35 +0200

    Initial Commit

---
+++ LayoutAndGeomtry/LayoutAndGeomtryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeomtryApp.swift
+//  LayoutAndGeomtry
+//
+//  Created by Kirill Streltsov on 06.08.23.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeomtryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
